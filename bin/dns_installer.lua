287cfe0a4aa740e02218ad1d24ab69d096c24c4d
SpaceBeeGaming <space.bee.gaming@gmail.com>
2018-11-01 21:19:50 +0200

    Installer script improvements

--- bin/dns_installer.lua
+++ bin/dns_installer.lua
@@ -3,7 +3,7 @@ local dlURL = "https://raw.githubusercontent.com/SpaceBeeGaming/OC-DNS/master/"
 
 local files = {
   server = {"bin/dns_server.lua", "bin/dns_server_starter.lua", "lib/tableToFile.lua"},
-  client = {"bin/dns_client.lua", "lib/tableToFile.lua"}
+  client = {"lib/dns_client.lua", "lib/tableToFile.lua"}
 }
 
 print("Choose what to install.")
@@ -11,6 +11,10 @@ print("[1] Server")
 print("[2] Client")
 local input = io.read()
 
+io.write("Are you sure? [Y/n]: ")
+if (io.read():lower() == "n") then --TEST: Is this allowed?
+  return
+end
 local port
 local settings = {}
 if (tostring(input) == "1") then
@@ -34,7 +38,7 @@ elseif (tostring(input) == "2") then
   end
   settings.port = tonumber(port) or 9999
 else
-  return
+  print("'" .. input .. "' is not '1' or '2'")
 end
 
 local settingsFile = io.open("/dns/DNS_SETTINGS.cfg", "w")
@@ -43,4 +47,18 @@ settingsFile:close()
 
 for i = 1, #files do
   os.execute("wget " .. dlURL .. i .. " /dns/" .. i)
+end
+if (tostring(input) == "1") then
+  local shellFile = io.open("$HOME/.shrc", "a")
+  shellFile:write("/dns/dns_server_starter.lua")
+  io.write("Installation finished. Do you want to start the server: [Y/n]: ")
+  if (io.read():lower() == "n") then
+    return
+  else
+    os.execute("/dns/bin/dns_server_starter.lua")
+    os.execute("rc dns enable")
+    os.execute("rc") --? Is this needed?
+  end
+else
+  print("Installation finished.")
 end
--- bin/dns_server_starter.lua
+++ bin/dns_server_starter.lua
@@ -1,7 +1,3 @@
-local os = require("os")
-
-os.execute("ln /programs/bin/dns_server.lua /etc/rc.d/dns.lua")
-os.execute("ln /programs/bin /usr/bin")
-os.execute("ln /programs/lib /usr/lib")
-os.setenv("HOSTNAME", "DNS-01")
+os.execute("ln /dns/bin/dns_server.lua /etc/rc.d/dns.lua")
+os.execute("ln /dns/lib /usr/lib")
 os.execute("rc")
