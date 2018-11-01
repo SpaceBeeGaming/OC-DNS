c8d0935e15955ea111114779a2e1bab648691f1d
SpaceBeeGaming <space.bee.gaming@gmail.com>
2018-11-01 17:33:23 +0200

    Add starter
    Added script for prepairing the server.

---
+++ bin/dns_server_starter.lua
@@ -0,0 +1,6 @@
+local os = require("os")
+
+os.execute("ln /programs/bin/dns_server.lua /etc/rc.d/dns.lua")
+os.execute("ln /programs/bin /usr/bin")
+os.execute("ln /programs/lib /usr/lib")
+os.setenv("HOSTNAME", "DNS-01")
