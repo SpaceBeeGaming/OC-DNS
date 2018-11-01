a7fa81e2a39a77b01215832f3f04ce0a3cf417ac
SpaceBeeGaming <space.bee.gaming@gmail.com>
2018-11-01 13:35:25 +0200

    Removed obsolite functions

--- bin/dns_client.lua
+++ bin/dns_client.lua
@@ -5,6 +5,7 @@ local ttf = require("tableToFile")
 
 local settingsLocation = "/usr/data/settings.cfg"
 local settings = ttf.load(settingsLocation)
+
 modem.open(settings.port)
 modem.broadcast(settings.port, "DNS", "DISCOVER")
 local pReply = event.pull(1, "modem_message") --_, rAddr, port, _, service, request, response
@@ -15,25 +16,8 @@ local reply = {
   port = pReply[3],
   service = pReply[5],
   request = pReply[6],
-  response = pReply[7]
+  response = {
+    value = pReply[7],
+    reason = pReply[8]
+  }
 }
-local internal = {}
-function internal.checkService()
-  if (reply.service == "DNS") then
-    return true
-  end
-end
-
-function internal.requestHandler()
-  for i in requests do
-    if (i == reply.request) then
-      return true
-    end
-  end
-end
-
-function internal.processReply()
-  if (internal.checkService()) then
-    internal.requestHandler()
-  end
-end
