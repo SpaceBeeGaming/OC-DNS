fddda8f50da27581699d3c821206539a78c89005
SpaceBeeGaming <space.bee.gaming@gmail.com>
2018-11-02 16:02:09 +0200

    Installer Script fixes pt.1

--- bin/dns_installer.lua
+++ bin/dns_installer.lua
@@ -1,4 +1,5 @@
 local serialization = require("serialization")
+local fs = require("filesystem")
 local dlURL = "https://raw.githubusercontent.com/SpaceBeeGaming/OC-DNS/master/"
 
 local files = {
@@ -18,6 +19,8 @@ end
 local port
 local settings = {}
 if (tostring(input) == "1") then
+  fs.makeDirectory("/dns/bin")
+  fs.makeDirectory("/dns/lib")
   files = files.server
   settings.LOG_FILE = "/dns/DNS_LOG.log"
   settings.HOST_FILE = "/dns/HOSTS.log"
@@ -30,6 +33,7 @@ if (tostring(input) == "1") then
   settings.port = tonumber(port) or 9999
 elseif (tostring(input) == "2") then
   files = files.client
+  fs.makeDirectory("/dns/lib")
 
   print("Port for DNS communication (default:'9999')")
   port = io.read()
@@ -41,16 +45,17 @@ else
   print("'" .. input .. "' is not '1' or '2'")
 end
 
+--fs.makeDirectory("/dns")
 local settingsFile = io.open("/dns/DNS_SETTINGS.cfg", "w")
 settingsFile:write(serialization.serialize(settings))
 settingsFile:close()
 
 for i = 1, #files do
-  os.execute("wget " .. dlURL .. i .. " /dns/" .. i)
+  os.execute("wget " .. dlURL .. files[i] .. " /dns/" .. files[i])
 end
 if (tostring(input) == "1") then
   local shellFile = io.open("$HOME/.shrc", "a")
-  shellFile:write("/dns/dns_server_starter.lua")
+  shellFile:write("/dns/bin/dns_server_starter.lua\n")
   io.write("Installation finished. Do you want to start the server: [Y/n]: ")
   if (io.read():lower() == "n") then
     return
